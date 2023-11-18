4411ede42d6883100bf39e770e2155cc19685a7a
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-18 18:30:13 +0530

    implement simple todo application with HTML, Tailwind

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request
+from flask_assets import Bundle, Environment
+from todos import todos
+
+load_dotenv()
 
 app = Flask(__name__)
 
+assets = Environment(app)
+css = Bundle("src/main.css", output="dist/main.css")
+js = Bundle("src/*.js", output="dist/main.js")
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+assets.register("css", css)
+assets.register("js", js)
+css.build()
+js.build()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search_todo():
+    search_term = request.form.get("search")
+
+    if not len(search_term):
+        return render_template("todo.html", todos=[])
+
+    res_todos = []
+    for todo in todos:
+        if search_term in todo["title"]:
+            res_todos.append(todo)
+
+    return render_template("todo.html", todos=res_todos)
