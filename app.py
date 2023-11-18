08d2420a91c457b0890b0dc266b5460c13906a15
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-18 22:46:26 +0530

    add supabase db

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from dotenv import load_dotenv
+import os
+from datetime import datetime
 
-from flask import Flask, render_template, request
+import jinja_partials
+from dotenv import load_dotenv
+from flask import Flask, render_template, request as req
 from flask_assets import Bundle, Environment
-from todos import todos
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id: int = db.Column("id", db.Integer, primary_key=True)
+    title: str = db.Column(db.String)
+    content: str = db.Column(db.String)
+
+    date_created: datetime = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"BlogPost(id={self.id!r}, title={self.title!r})"
+
+
+with app.app_context():
+    db.create_all()
+
+jinja_partials.register_extensions(app)
 
 assets = Environment(app)
 css = Bundle("src/main.css", output="dist/main.css")
@@ -18,21 +42,21 @@ css.build()
 js.build()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    match req.method:
+        case "POST":
+            blog = BlogPost(title=req.form["title"], content=req.form["content"])
+            db.session.add(blog)
+            db.session.commit()
+            return jinja_partials.render_partial("blog/blog_card.html", blog=blog)
+
+        case "GET":
+            blogs: list[BlogPost] = db.session.execute(db.select(BlogPost).order_by(BlogPost.id)).scalars()
+            return render_template("home/index.html", blogs=blogs)
 
 
-@app.route("/search", methods=["POST"])
-def search_todo():
-    search_term = request.form.get("search")
-
-    if not len(search_term):
-        return render_template("todo.html", todos=[])
-
-    res_todos = []
-    for todo in todos:
-        if search_term in todo["title"]:
-            res_todos.append(todo)
-
-    return render_template("todo.html", todos=res_todos)
+@app.route('/blog/<int:blog_id>')
+def view_post(blog_id: int):
+    blog: BlogPost = db.get_or_404(BlogPost, blog_id)
+    return render_template('blog/view_blog.html', blog=blog)
